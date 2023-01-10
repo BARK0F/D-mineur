0a37bc9e72f07a49e2971778794c0201d5e26abc
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-01-10 09:36:16 +0100

    Coordonnées implémentées : test ok

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -22,7 +22,28 @@ def construireCoordonnee(li :int, co :int)->tuple:
     '''
     la fonction retourne un tuple qui contient le num de ligne et le num de colonne passés en paramètre
     '''
-    res = (li,co)
-    return res
+    if type(li) != int or type(li) != int :
+        raise TypeError(f"construireCoordonnee : Le numéro de ligne {type(li)} ou le numéro de colonne {type(co)} ne sont pas des entiers")
+    elif li < 0 or co < 0 :
+        raise ValueError(f"construireCoordonnee : Le numéro de ligne {li} ou de colonne {co} ne sont pas positifs")
+    else:
+        res = (li,co)
+        return res
 
+def getLigneCoordonnee(coord:tuple):
+    '''
+    retourne le numéro de ligne de la coordonnée passée en paramètre
+    '''
+    if type(coord) != tuple:
+        raise TypeError("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
+    else:
+        return coord[0]
+def getColonneCoordonnee(coord:tuple):
+    '''
+    retourne le numéro de colonne de la coordonnée passée en paramètre
+    '''
+    if type(coord) != tuple :
+        raise TypeError("getColonneCoordonnee : Le paramètre n’est pas une coordonnée")
+    else:
+        return coord[1]
 
