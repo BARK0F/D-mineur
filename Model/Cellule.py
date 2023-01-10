f9825800a42f8a0890aeaaec646154e46f3d9dba
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2023-01-10 12:03:23 +0100

    getters/setters de cellule : test ok

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -28,11 +28,63 @@ def isContenuCorrect(nb:int)->bool:
     return isinstance(nb,int) and 0<= nb <= 8 or nb == const.ID_MINE
 
 def construireCellule(contenu:int=0, visible:bool=False):
+    '''
+    retourne un dictionnaire qui fera office de cellule
+    '''
     if isContenuCorrect(contenu) == False:
         raise ValueError(f"construireCellule : le contenu {contenu} n’est pas correct")
     if type(visible) != bool:
         raise TypeError(f"construireCellule : le second paramètre {visible} n’est pas un booléen")
-    dict = {}
-    dict[const.CONTENU] = contenu
-    dict[const.VISIBLE] = visible
-    return dict
+    dic = {}
+    dic[const.CONTENU] = contenu
+    dic[const.VISIBLE] = visible
+    return dic
+
+def getContenuCellule(dic:dict)->int:
+    '''
+    retourne le contenu d'une cellule
+    '''
+    if type_cellule(dic)==False:
+        raise TypeError("« getContenuCellule : Le paramètre n’est pas une cellule")
+    return dic[const.CONTENU]
+
+def isVisibleCellule(dic:dict)->bool:
+    '''
+    retourne si la cellule est visible ou pas
+    '''
+    if type_cellule(dic)==False:
+        raise TypeError("isVisibleCellule : Le paramètre n’est pas une cellule")
+    return dic[const.VISIBLE]
+
+def setContenuCellule(dic:dict, contenu:int)->None:
+    '''
+    ne retourne rien, modifie avec le deuxième paramètre le contenue de la cellule
+    '''
+    if type_cellule(dic)==False:
+        raise TypeError("setContenuCellule : Le premier paramètre n’est pas une cellule")
+    if type(contenu)!=int:
+        raise TypeError("setContenuCellule : Le second paramètre n’est pas un entier")
+    if isContenuCorrect(contenu) == False:
+        raise ValueError(f"construireCellule : le contenu {contenu} n’est pas correct")
+    dic[const.CONTENU] = contenu
+    return None
+
+def setVisibleCellule(dic:dict,bol:bool)->None:
+    '''
+    ne retourne rien, modifie avec le deuxième paramètre la visibilité de la cellule
+    '''
+    if type_cellule(dic)==False:
+        raise TypeError("setVisibleCellule : Le premier paramètre n’est pas une cellule")
+    if type(bol)!=bool:
+        raise TypeError("setVisibleCellule : Le second paramètre n’est pas un booléen")
+    dic[const.VISIBLE] = bol
+    return None
+
+def contientMineCellule(dic:dict)->bool:
+    if type_cellule(dic)==False:
+        raise TypeError("contientMineCellule : Le paramètre n’est pas une cellule")
+    if dic[const.CONTENU] == const.ID_MINE:
+        bol = True
+    else :
+        bol = False
+    return bol
